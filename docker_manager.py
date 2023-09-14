e09b73ec70acf0594328f0c153958371989cf944
greedydurian <aaronoh2015@gmail.com>
2023-09-14 22:28:37 +0800

    added logs and comment

--- docker_manager.py
+++ docker_manager.py
@@ -2,9 +2,16 @@ import docker
 import logging
 
 class DockerManager:
-
     def __init__(self, client=None):
         self.client = client or docker.from_env()
+
+    # check if the target image exists
+    def image_exists(self, image_name):
+        try:
+            self.client.images.get(image_name)
+            return True
+        except docker.errors.ImageNotFound:
+            return False
 
     def stop_and_remove_container(self, container_name):
         try:
@@ -17,9 +24,16 @@ class DockerManager:
             logging.warning(f"Container {container_name} not found")
             return False
 
-    def start_new_container(self, container_name, image):
+    def start_new_container(self, container_name, image, volume_mounts=[]):
+        """
+        Starts a new container with the given image and name 
+        :param container_name: Name of the container to start
+        :param image: Docker image to use for the new container
+        :param volume_mounts: List of volume mounts to attach to the container
+        :return: True if the container starts successfully, False otherwise
+        """
         try:
-            new_container = self.client.containers.run(image, name=container_name, detach=True)
+            new_container = self.client.containers.run(image, name=container_name, detach=True, mounts=volume_mounts)
             logging.info(f"Successfully started new container {container_name} with image {image}")
             return True
         except docker.errors.ImageNotFound:
@@ -28,3 +42,18 @@ class DockerManager:
         except Exception as e:
             logging.error(f"An error occurred: {e}")
             return False
+
+
+    def get_container_volume_details(self, container_name):
+        """
+        Retrieve the volume details for a given container.
+        
+        :param container_name: Name of the container to inspect.
+        :return: A list of volume mounts if the container exists, an empty list otherwise.
+        """
+        try:
+            container = self.client.containers.get(container_name)
+            volume_mounts = container.attrs['HostConfig']['Mounts']
+            return volume_mounts
+        except docker.errors.NotFound:
+            return []
--- main.py
+++ main.py
@@ -3,13 +3,17 @@
 import argparse
 import logging
 import json
-from docker_manager import DockerManager  
+from docker_manager import DockerManager
 
 def setup_logging():
     logHandler = logging.FileHandler("logfile.txt")
     logger = logging.getLogger()
     logger.addHandler(logHandler)
     logger.setLevel(logging.INFO)
+    
+    ## logs into console
+    console_handler = logging.StreamHandler()
+    logger.addHandler(console_handler)
 
 def json_log(message, level="INFO"):
     log_entry = json.dumps({"level": level, "message": message})
@@ -18,12 +22,16 @@ def json_log(message, level="INFO"):
     elif level == "ERROR":
         logging.error(log_entry)
 
-def rollback_image(container_name, target_image, force=False):
+def rollback_image(container_name, target_image, force=False, preserve_volumes=False):
     docker_manager = DockerManager()
 
     if not docker_manager.image_exists(target_image):
         json_log(f"Target image {target_image} not found. Aborting rollback.", level="ERROR")
         return
+
+    volume_mounts = []
+    if preserve_volumes:
+        volume_mounts = docker_manager.get_container_volume_details(container_name)
 
     user_input = input(f"Are you sure you want to stop and remove the container {container_name}? (y/n): ")
     if user_input.lower() != 'y' and not force:
@@ -35,7 +43,7 @@ def rollback_image(container_name, target_image, force=False):
     else:
         json_log(f"Container {container_name} not found. Will attempt to start new container.")
 
-    if docker_manager.start_new_container(container_name, target_image):
+    if docker_manager.start_new_container(container_name, target_image, volume_mounts):
         json_log(f"Started new container {container_name} with image {target_image}")
     else:
         json_log(f"Failed to start new container {container_name} with image {target_image}", level="ERROR")
@@ -44,7 +52,8 @@ if __name__ == "__main__":
     setup_logging()
     parser = argparse.ArgumentParser(description="Docker image rollback tool")
     parser.add_argument("container_name", type=str, help="Name of the container to rollback")
+    parser.add_argument("--preserve-volumes", action="store_true", help="Preserve volumes during rollback")
     parser.add_argument("target_image", type=str, help="Target image to rollback to")
     parser.add_argument("--force", action="store_true", help="Forcefully stop and remove running containers")
     args = parser.parse_args()
-    rollback_image(args.container_name, args.target_image, force=args.force)
+    rollback_image(args.container_name, args.target_image, force=args.force, preserve_volumes=args.preserve_volumes)
